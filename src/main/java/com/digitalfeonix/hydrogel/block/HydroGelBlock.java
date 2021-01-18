f0c78ae689f29a8c6cd76f305b157276024e4a25
Shadows_of_Fire <Bward7864@gmail.com>
2021-01-18 14:26:24 -0500

    Closes #1

--- src/main/java/com/digitalfeonix/hydrogel/block/HydroGelBlock.java
+++ src/main/java/com/digitalfeonix/hydrogel/block/HydroGelBlock.java
@@ -3,16 +3,21 @@ package com.digitalfeonix.hydrogel.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.block.ILiquidContainer;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.PushReaction;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
-public class HydroGelBlock extends Block {
+public class HydroGelBlock extends Block implements ILiquidContainer {
 
 	public static final IntegerProperty LEVEL = FlowingFluidBlock.LEVEL;
 
@@ -38,6 +43,16 @@ public class HydroGelBlock extends Block {
 	@Override
 	public FluidState getFluidState(BlockState p_204507_1_) {
 		return Fluids.WATER.getDefaultState();
+	}
+
+	@Override
+	public boolean canContainFluid(IBlockReader worldIn, BlockPos pos, BlockState state, Fluid fluidIn) {
+		return false;
+	}
+
+	@Override
+	public boolean receiveFluid(IWorld worldIn, BlockPos pos, BlockState state, FluidState fluidStateIn) {
+		return false;
 	}
 
 }
